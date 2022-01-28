ffb63197886875d6f3b38b3d90ff4cbf03cbde63
OlehS <olehjs16@gmail.com>
2022-01-28 22:22:19 +0200

    Updates

---
+++ precache-manifest.42116ac00f2fc1ac5d3ac48c70f7a2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df699a1fc018c7bff19724d40f3ec8c",
+    "url": "/react-speed-typing-game/index.html"
+  },
+  {
+    "revision": "18a984aeffbf2995b77c",
+    "url": "/react-speed-typing-game/static/css/main.37f67fd6.chunk.css"
+  },
+  {
+    "revision": "7b4d9399ab4e1bfee2d9",
+    "url": "/react-speed-typing-game/static/js/2.9b973634.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-speed-typing-game/static/js/2.9b973634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a984aeffbf2995b77c",
+    "url": "/react-speed-typing-game/static/js/main.47aaf79e.chunk.js"
+  },
+  {
+    "revision": "95dca032e7627b6b93b1",
+    "url": "/react-speed-typing-game/static/js/runtime-main.ca52b0b1.js"
+  }
+]);
